f5c724bce6b235163e462bde360f524b36f8fa9e
Lucas Lima <lucaslimalvs@hotmail.com>
2019-04-18 08:54:35 -0300

    Criar arquivos index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let emocionalDaAna = document.querySelector("p");
+let titulo = document.querySelector("h1");
+let corpo = document.querySelector("body");
+let imagem = document.querySelector("img");
+//innerHTML=acessa o conteudo do html
+titulo.innerHTML = "Emocional da Ana é aqui";
+titulo.style.color = "blue";
+
+emocionalDaAna.style.fontSize = "15pt";
+corpo.style.backgroundColor = "salmon";
+
+imagem.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/51/disappointed-but-relieved-face_1f625.png";
+
